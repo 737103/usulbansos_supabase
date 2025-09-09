658cd8b4a13daac69921926dc79c2d4e927271c4
737103 <barabarayaselatan96@gmail.com>
2025-09-09 16:19:35 +0800

    33

--- assets/js/script.js
+++ assets/js/script.js
@@ -2324,7 +2324,7 @@ async function showRiwayatAjuan() {
         content.innerHTML = `
             <div class="table-container">
                 <h3>Riwayat Ajuan Bantuan Sosial</h3>
-                <table class="table" style="width: 100%;">
+                <table class="table riwayat-ajuan-table" style="width: 100%;">
                     <thead>
                         <tr>
                             <th>Jenis Bantuan</th>
